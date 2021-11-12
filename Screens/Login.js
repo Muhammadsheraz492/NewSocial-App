7a9009a1f1f109d67c80d803977adf44dc97a093
Muhammadsheraz492 <muhammadshiraz492@gmail.com>
2021-11-12 02:08:14 -0800

    Setting of React-native-vector

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+// import Icon from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const Login = () => {
   return (
-    <View>
+    <View style={{alignItems: 'center'}}>
       <Text>Ths Login Screen</Text>
+      {/* <Icon name="md-menu" size={30} /> */}
+      <AntDesign name="user" size={30} />
+      <Text>gfvgf</Text>
     </View>
   );
 };
