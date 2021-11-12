05a844aa4ff19d097e5335b76ee2af6c03512c9f
Muhammadsheraz492 <muhammadshiraz492@gmail.com>
2021-11-12 10:48:48 -0800

    TextInput Completed

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-// import Icon from 'react-native-vector-icons/Ionicons';
+
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const Login = () => {
   return (
-    <View style={{alignItems: 'center'}}>
-      <Text>Ths Login Screen</Text>
-      {/* <Icon name="md-menu" size={30} /> */}
-      <AntDesign name="user" size={30} />
-      <Text>gfvgf</Text>
+    <View
+      style={{
+        top: '10%',
+        // alignItems: 'center',
+        justifyContent: 'center',
+        // flexDirection: 'row',
+        width: '90%',
+        alignSelf: 'center',
+        height: '8%',
+        // backgroundColor: 'black',
+        borderWidth: 2,
+        // alignItems:''
+      }}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{justifyContent: 'center', left: '9%'}}>
+          <AntDesign name="user" size={30} />
+        </View>
+        <View
+          style={{
+            // borderWidth: 2,
+            // alignItems:''
+            height: 55,
+            borderLeftWidth: 2,
+            // alignItems: 'center',
+            left: '13%',
+            justifyContent: 'center',
+          }}>
+          <Text>gfvgf</Text>
+        </View>
+      </View>
     </View>
   );
 };
