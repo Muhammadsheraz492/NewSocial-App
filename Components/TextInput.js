b605b48e9cd2bf9eb054e9f921a31c1659e6533d
Muhammadsheraz492 <muhammadshiraz492@gmail.com>
2021-11-13 10:18:11 -0800

    Responsive Screen Login And Sign up

--- Components/TextInput.js
+++ Components/TextInput.js
@@ -1,16 +1,50 @@
+import {placeholder} from '@babel/types';
 import React from 'react';
 import {View, Text} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 
-// import FontAwesome from 'react-native-vector-icons/FontAwesome';
-const TextInput = () => {
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const TextInputScreen = ({IconName, IconType, Color, placeholder}) => {
   return (
-    <View>
-      <View style={{alignItems: 'center'}}>{/* <FontAwesome /> */}</View>
-      <View>
-        <Text>bucyrvc</Text>
+    <View
+      style={{
+        top: '10%',
+        // alignItems: 'center',
+        justifyContent: 'center',
+        // flexDirection: 'row',
+        width: '90%',
+        alignSelf: 'center',
+        height: '8%',
+        // backgroundColor: 'black',
+        borderWidth: 1,
+        // alignItems:''
+        // borderColor: '#fff',
+      }}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{justifyContent: 'center', left: '9%'}}>
+          <AntDesign name={IconType} size={30} />
+        </View>
+        <View
+          style={{
+            // borderWidth: 2,
+            // alignItems:''
+            height: 55,
+            borderLeftWidth: 1,
+            // alignItems: 'center',
+            left: '20%',
+            justifyContent: 'center',
+            // borderColor: '#fff',
+          }}>
+          <TextInput
+            style={{left: 10}}
+            placeholder={placeholder}
+            value={IconName}
+          />
+        </View>
       </View>
     </View>
   );
 };
 
-export default TextInput;
+export default TextInputScreen;
