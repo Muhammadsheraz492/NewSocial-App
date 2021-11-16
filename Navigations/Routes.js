ab6840ca31546108834f64ef16feee42d5f025c7
Muhammadsheraz492 <90733116+Muhammadsheraz492@users.noreply.github.com>
2021-11-15 23:44:57 -0800

    Firebase Store Completed

--- Navigations/Routes.js
+++ Navigations/Routes.js
@@ -2,6 +2,8 @@
 
 import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native-animatable';
+import firestore from '@react-native-firebase/firestore';
+
 import {NavigationContainer} from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
 import AppStack from './AppStack';
@@ -13,10 +15,26 @@ function RouteScreen() {
     setUser(user);
     if (initializing) setInitializing(false);
   }
+  const data = async () => {
+    firestore().collection('users').doc('wezXL6bvUelwWbkShqpF').delete();
+    // .update({
+    //   passwd: 'new paaswd',
+    // })
+    // .then(e => console.log('Updated'));
+    // const usersCollection = await firestore().collection('users').get();
+    // // .doc('wezXL6bvUelwWbkShqpF')
+    // // .get();
+    // // console.log(usersCollection.docs);
+    // usersCollection.forEach(element => {
+    //   console.log(element.data());
+    // });
+  };
 
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    // const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    // return subscriber; // unsubscribe on unmount
+
+    data();
   }, []);
 
   if (initializing) return null;
