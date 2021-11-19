00711c4b60af27dbba9eefe4cafc7e65408ae62c
Muhammadsheraz492 <90733116+Muhammadsheraz492@users.noreply.github.com>
2021-11-19 11:42:20 -0800

    Color Store in Redux

--- Redux/counterSlice.js
+++ Redux/counterSlice.js
@@ -4,25 +4,20 @@ export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
     value: 0,
+    backgroundColor: '#fff',
+    textColor: 'black',
   },
   reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    changeBackgroundColor: (state, action) => {
+      state.backgroundColor = action.payload;
     },
-    decrement: state => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    ChangeTextColor: (state, action) => {
+      state.textColor = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const {increment, decrement, incrementByAmount} = counterSlice.actions;
+export const {ChangeTextColor, changeBackgroundColor} = counterSlice.actions;
 
 export default counterSlice.reducer;
